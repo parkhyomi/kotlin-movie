870375ab80b0bc0abe9a614b25f1f04cdfd3c713
parkhyomi <phyomi3@naver.com>
2026-04-14 16:19:11 +0900

    refactor: SeatClass에 price 속성 추가 및 가격 계산 로직 수정

--- src/main/kotlin/domain/model/cart/CartItem.kt
+++ src/main/kotlin/domain/model/cart/CartItem.kt
@@ -2,16 +2,14 @@ package domain.model.cart
 
 import domain.model.screen.Screening
 import domain.model.seat.Seat
-import domain.model.seat.SeatClass
 
-// 장바구니 항목 1건(상영 + 선택 좌석 목록)을 표현한다.
 data class CartItem(
     val screening: Screening,
     val seats: List<Seat>,
 ) {
-    // 장바구니 항목 1건의 좌석 총 금액을 계산한다.
+
     fun seatAmount(): Int =
         seats.sumOf { seat ->
-            SeatClass.toPrice(seat.seatClass)
+            seat.seatClass.price
         }
 }
--- src/main/kotlin/domain/model/seat/Seat.kt
+++ src/main/kotlin/domain/model/seat/Seat.kt
@@ -1,6 +1,5 @@
 package domain.model.seat
 
-// 좌석은 row(알파벳) + column(숫자) + 좌석 등급으로 구성된다.
 data class Seat(
     val column: Int,
     val row: RowLabel,
@@ -12,19 +11,20 @@ data class Seat(
     }
 
     companion object {
-        // 좌석 번호 범위: 1 ~ 12
         val columns = (1..12).toList()
 
-        // 좌석 행 범위: A ~ E
         val rows = RowLabel.entries
     }
 }
 
 // 좌석 등급
-enum class SeatClass {
-    B,
-    A,
-    S, ;
+enum class SeatClass(
+    val price: Int,
+) {
+    B(12_000),
+    A(15_000),
+    S(18_000),
+    ;
 
     companion object {
         // 행 기준으로 좌석 등급을 결정한다.
@@ -33,14 +33,6 @@ enum class SeatClass {
                 RowLabel.A, RowLabel.B -> B
                 RowLabel.C -> A
                 RowLabel.D, RowLabel.E -> S
-            }
-
-        // 좌석 등급의 가격을 반환한다.
-        fun toPrice(seatClass: SeatClass): Int =
-            when (seatClass) {
-                B -> 12000
-                A -> 15000
-                S -> 18000
             }
     }
 }
