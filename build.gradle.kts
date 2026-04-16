9b75943764a626e44a6c8938e3c52e0d032426bf
parkhyomi <phyomi3@naver.com>
2026-04-16 16:19:31 +0900

    build: H2 데이터베이스 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.14.3")
     testImplementation("org.assertj", "assertj-core", "3.27.7")
     testImplementation("io.kotest", "kotest-runner-junit5", "6.0.7")
+
+    runtimeOnly("com.h2database:h2:2.4.240")
 }
 
 tasks {
