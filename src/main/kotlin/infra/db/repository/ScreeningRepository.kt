535ef74cc1ca33e32d0ce8e904f3703ae0114de2
parkhyomi <phyomi3@naver.com>
2026-04-16 17:44:29 +0900

    feat: ScreeningRepository 인터페이스 추가

---
+++ src/main/kotlin/infra/db/repository/ScreeningRepository.kt
@@ -0,0 +1,33 @@
+package infra.db.repository
+
+import domain.model.screeningschedule.Screening
+import domain.model.seat.Seat
+import domain.model.seat.SeatAvailability
+import java.time.LocalDate
+import java.time.LocalTime
+
+interface ScreeningRepository{
+    fun findAllScreenings(): List<Screening>
+
+    fun saveAll(screenings: List<Screening>)
+    fun screeningsOfMovieTitle(movieTitle: String): List<Screening>
+    fun screeningsOfMovieDate(screenings: List<Screening>, date: LocalDate): List<Screening>
+    fun seatStatusesOf(
+        movieTitle: String,
+        date: LocalDate,
+        startTime: LocalTime
+    ): List<SeatAvailability>
+
+    fun reserveSeats(
+        movieTitle: String,
+        date: LocalDate,
+        startTime: LocalTime,
+        seats: List<Seat>
+    ): Screening
+
+    fun createScreening(
+        movieTitle: String,
+        screeningDate: LocalDate,
+        startTime: LocalTime
+    ): Screening
+}
