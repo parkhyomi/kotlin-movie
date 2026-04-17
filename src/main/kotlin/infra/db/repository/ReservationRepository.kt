d97d59f179a54174595d1c8f565d67b3a75e7ea9
parkhyomi <phyomi3@naver.com>
2026-04-17 13:50:10 +0900

    feat: ReservationRepository 인터페이스 추가

---
+++ src/main/kotlin/infra/db/repository/ReservationRepository.kt
@@ -0,0 +1,12 @@
+package infra.db.repository
+
+import domain.model.seat.Seat
+
+interface ReservationRepository {
+    fun findReservedSeats(screeningId: Long): List<Seat>
+
+    fun reserveSeats(
+        screeningId: Long,
+        seats: List<Seat>,
+    )
+}
